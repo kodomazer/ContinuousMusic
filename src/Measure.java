9f59fdd00286fb54e98844c24e374cc2d804cb07
ambersz <ambersz@gmail.com>
2014-11-08 22:27:03 -0800

    Create Measure.java

---
+++ src/Measure.java
@@ -0,0 +1,20 @@
+public class Measure {
+        public int tempo;
+        public LinkedList() noteList;
+        
+        public Measure(int tempo, LinkedList() noteList);
+        public Measure() {
+                tempo = 0;
+                noteList = new LinkedList();
+        }
+        
+        
+        public void setTempo(int newValue) {
+                tempo = newValue;
+        }
+        public void editNote(int index,Note newNote) {
+                noteList.set(index, newNote);
+        }
+        public void addNote(Note newNote) {
+                noteList.set(index, newNote)
+        }
