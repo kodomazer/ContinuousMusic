bb802a62708f61f63450f2774179fee5e8daa6c8
kodomazer <brianzha@gmail.com>
2014-11-23 18:01:31 -0800

    Quick Update
    Committing changes so I can change the repository location on my
    computer.

--- src/RawNote.java
+++ src/RawNote.java
@@ -38,6 +38,50 @@ public:
 		return tick;
 	}
 	
+	boolean getCommand(){
+		if(this.commandNum==0x80)
+			return true;
+		else
+			return false;
+	}
+	
+	//Setters
+	boolean setChannel(int newChannel){
+		if(newChannel>=0&&newChannel<16)
+			this.channel=newChannel;
+		else
+			return false;
+		return true;
+	}
+	boolean setKey(int newKey){
+		if(newKey>=0&&newKey<128)
+			this.key=newKey;
+		else
+			return false;
+		return true;
+	}
+	boolean setVelocity(int newVelocity){
+		if(newVelocity>=0||newVelocity<128)
+			this.velocity=newVelocity;
+		else
+			return false;
+		return true;
+	}
+	boolean setTick(int newTick){
+		if(newTick>=0)
+			this.tick=newTick;
+		else
+			return false;
+		return true;
+	}
+	
+	void setCommand(boolean on){
+		if(on)
+			commandNum=0x90;
+		else
+			commandNum=0x80;
+	}
+	
 	//returns a MidiCommand, that represents the RawNote, to be used in the player
 	public ShortMessage toShortMessage(){
 		try{
